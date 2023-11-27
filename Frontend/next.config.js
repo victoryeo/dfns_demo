8e8f628349ca94b61de311019bda4121ab7abf05
yeosiowvic <victoryeo1974@gmail.com>
2023-11-27 20:38:07 +0800

    update next config

--- Frontend/next.config.js
+++ Frontend/next.config.js
@@ -1,10 +1,8 @@
-module.exports = {
-    reactStrictMode: true,
-    env: {
-      NX_WALLET_CONNECT_ID: process.env.NX_WALLET_CONNECT_ID,
-      NX_FIREBLOCKS_API_KEY: process.env.NX_FIREBLOCKS_API_KEY,
-      NX_FIREBLOCKS_API_PRIVATE_KEY_PATH: process.env.NX_FIREBLOCKS_API_PRIVATE_KEY_PATH,
-      NX_FIREBLOCKS_VAULT_ACCOUNT_IDS: process.env.NX_FIREBLOCKS_VAULT_ACCOUNT_IDS,
-    }
-  }
-  
+const result = require('dotenv').config({ path: '.env.local' })
+
+const nextConfig = {
+  env: result.parsed,
+  transpilePackages: ['@dfns/sdk-webauthn'],
+}
+
+module.exports = nextConfig
